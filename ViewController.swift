a3fd39c20b29bc8632bc7aacf591f7c0d5ba28b3
Samuel Noye <samuel.noye@amalitech.org>
2022-07-02 17:32:42 +0000

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -44,7 +44,7 @@ class ViewController: UIViewController {
     
     // MARK: - Custom Functions
     func addDummyData() {
-        options.append(RideOption(title: "Speed meter", image: "promotions"))
+        options.append(RideOption(title: "Speed", image: "promotions"))
         options.append(RideOption(title: "Tunnel", image: "promotions"))
         options.append(RideOption(title: "Curves", image: "promotions"))
         options.append(RideOption(title: "Forest", image: "promotions"))
